e9d7cd804522a32278bbcfe328397387250618e5
Julian Avendaño <julian.arkangel@gmail.com>
2020-04-09 05:18:53 -0400

    Se configura el puerto con una variable global en el app.js. Se anexa morgan como middleware para visualizar las peticiones http en la terminal

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ const app = express();
 // Se configura la primera ruta, la base
 app.get('/', function (req, res) {
     // Se retorna un hola mundo al cargar la ruta
-    res.send('Hello World!');
+    res.send('Hello World! Julian Avendaño');
 });
 
-// Se configura el puerto para que escuche en el puerto 3000
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+// Se configura como variable global el puerto que le asigne el server o el 3000 sino
+app.set('puerto', process.env.PORT || 3000);
+// Se configura el puerto accediendo a la variable global
+app.listen(app.get('puerto'), function () {
+    console.log('Example app listening on port' + app.get('puerto'));
 });
+
+// Se coloca a que express use a morgan como middleware
+// Con morgan se visualiza en la consola las peticiones que se le hagan al servidor
+const morgan = require('morgan');
+app.use(morgan('tiny'));
+
+
+
+
+
+
+// ###################### NOTAS ##############################################
+// Para correr el servidor es con el comando node NOMBREDELARCHIVO.js (app.js)}
+// Se instala nodemon para autoreload del servidor, se configuro en package.json con "dev": "nodemon app.js", en scripts
