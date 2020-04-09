7b8410026f226d8e1418a9eb326794ac5c1fbd62
Julian Avendaño <julian.arkangel@gmail.com>
2020-04-09 06:15:15 -0400

    Instalacion de babel y anexo del mismo a la seccion de scripts de package.json para cambiar la sintaxis de los archivos del proyecto a un esquema de js mas nuevo y se corre el proyecto con npm run devbabel. Se elimino test de los escrips en package.json. Se migro el app.js a esquema nuevo y se deja comentada la ruta momentaneamente para que tome por default puclic

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 //Requerimos e intanciamos express
-const express = require('express');
-const app = express();
+import express from 'express'
 // Se coloca a que express use a morgan como middleware y muestre en consola las peticiones http
-const morgan = require('morgan');
+import morgan from 'morgan'
 // Cors configura el server para que no bloque las peticiones con otros dominios
-const cors = require('cors');
+import cors from 'cors'
 // Retorna la ruta especifica
-const path = require('path');
+import path from 'path'
 
-// morgan se usa como middleware
+const app = express();
+
+// ------------- MIDDLEWARES ---------------
 app.use(morgan('tiny'));
-
-// Express usa cors para peticiones de otras urls
 app.use(cors());
-
-// Se usa Json para que pueda trabajar con solicitudes o respuestas www-form-urlencoded
+// Se usa Json para que pueda trabajar con solicitudes
+// o respuestas www-form-urlencoded
 app.use(express.json());
 //application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
 
-// Se configura la primera ruta, la base
-app.get('/', function (req, res) {
-    // Se retorna un hola mundo al cargar la ruta
-    res.send('Hello World! Julian Avendaño');
-});
+// -------------- RUTAS ----------------------
+// app.get('/', function (req, res) {
+//     res.send('Hello World! Julian Avendaño');
+// });
 
-// Middleware para Vue.js router modo history
+// Middleware para Vue.js router modo history (Debe estar antes del puerto)
 const history = require('connect-history-api-fallback');
 app.use(history());
 // Para acceder al directorio public con la config del server
@@ -38,16 +36,10 @@ app.listen(app.get('puerto'), function () {
     console.log('Example app listening on port' + app.get('puerto'));
 });
 
-
-
-
-
-
-
-
 // ###################### NOTAS ##############################################
 // Para correr el servidor es con el comando node NOMBREDELARCHIVO.js (app.js)}
 // Tambien se corre con nodemon (Segun config) con npm run dev
+// Para correr con babel se ejecuta npm run devbabel (Para trabajar con js esquema nuevo)
 // Se instala nodemon para autoreload del servidor, se configuro en package.json con "dev": "nodemon app.js", en scripts
 // Con morgan se visualiza en la consola las peticiones que se le hagan al servidor
-// Express static se configura con el pach
+// Express static se configura con el pach del directirio public
