8d593ced8950ac41a5db2cc93ff0ce3fe0a89941
Julian Avendaño <julian.arkangel@gmail.com>
2020-04-09 05:42:03 -0400

    Se instala y configura morgan como middleware de express para que muestre en la terminal las peticiones http que se le realicen al server. Se instala y configura Cors para manejar solicitudes o respuestas de otros dominios y evitar los bloqueos por defectos (por cors). Se configura Json para que express trabaje con solicitudes y respuestas www-form-urlencoded

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 //Requerimos e intanciamos express
 const express = require('express');
 const app = express();
+// Se coloca a que express use a morgan como middleware y muestre en consola las peticiones http
+const morgan = require('morgan');
+// Cors configura el server para que no bloque las peticiones con otros dominios
+const cors = require('cors');
+
+// morgan se usa como middleware
+app.use(morgan('tiny'));
+
+// Express usa cors para peticiones de otras urls
+app.use(cors());
+
+// Se usa Json para que pueda trabajar con solicitudes o respuestas www-form-urlencoded
+app.use(express.json());
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
 
 // Se configura la primera ruta, la base
 app.get('/', function (req, res) {
@@ -15,10 +31,7 @@ app.listen(app.get('puerto'), function () {
     console.log('Example app listening on port' + app.get('puerto'));
 });
 
-// Se coloca a que express use a morgan como middleware
-// Con morgan se visualiza en la consola las peticiones que se le hagan al servidor
-const morgan = require('morgan');
-app.use(morgan('tiny'));
+
 
 
 
@@ -27,4 +40,6 @@ app.use(morgan('tiny'));
 
 // ###################### NOTAS ##############################################
 // Para correr el servidor es con el comando node NOMBREDELARCHIVO.js (app.js)}
+// Tambien se corre con nodemon (Segun config) con npm run dev
 // Se instala nodemon para autoreload del servidor, se configuro en package.json con "dev": "nodemon app.js", en scripts
+// Con morgan se visualiza en la consola las peticiones que se le hagan al servidor
